cb41c21becf631c5c585909af66590cdb08bd043
Rahul Kumar <kr.rahul2822@gmail.com>
2025-01-29 13:32:54 +0530

    first

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const commentRoutes = require('./routes/comments');
+
+dotenv.config();
+const app = express();
+app.use(cors({origin: '*',}));
+app.use(bodyParser.json());
+
+// mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('Error connecting to MongoDB:', err));
+app.use('/comments', commentRoutes);
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
